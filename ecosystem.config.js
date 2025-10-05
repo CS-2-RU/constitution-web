84e5b3be90962f0cd04c6cd9e2d38cc45db9f023
krevvetka <krvvko@gmail.com>
2025-10-05 02:27:26 -0400

    meow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,15 @@
 module.exports = {
     apps: [{
-        name: 'constitution.krvvko.me',
+        name: 'constitution.cs2ru.online',
         script: 'npm',
         args: 'run start',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
         env: {
             PORT: 3015,
+            NODE_OPTIONS: '--max-old-space-size=2048'
         },
     }]
 }
