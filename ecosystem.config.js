dc7f5620f4f4129fae4567fc6390d8b07ac00e2d
krevvetka <krvvko@gmail.com>
2025-11-08 22:52:55 -0500

    build update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,15 +3,9 @@ module.exports = {
         name: 'cs2ru.online',
         script: 'pnpm',
         args: 'run start',
-        cwd: '/root/constitution.cs2ru.online',
         env: {
-            PORT: 3015,
+            PORT: 3001,
             NODE_ENV: 'production'
         },
-        instances: 1,
-        exec_mode: 'fork',
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '500M'
     }]
 }
